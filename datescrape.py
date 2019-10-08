d38459ad6646b3221d6147f3097b31be109ff66c
Flecktarn <jacobtidd@gmail.com>
2019-10-09 03:24:12 +1000

    initial commit

---
+++ datescrape.py
@@ -0,0 +1,43 @@
+import sys
+import datetime
+
+def parseWeekDay(dayString):
+	#returns the day number (from 0 to 6) from a string representing the day.
+	dayIndex = [['m','mon','monday'],\
+			   ['tu','tue','tues','tuesday'],\
+			   ['w','wed','wednesday'],\
+			   ['th','thurs','thursday'],\
+			   ['f','fri','friday'],\
+			   ['sat','saturday'],\
+			   ['sun','sunday']]
+
+	for i in range(len(dayIndex)):
+		for j in range(len(dayIndex[i])):
+			if dayIndex[i][j] == dayString.lower():
+				return i
+
+	return -1
+
+try:
+	parseMe = sys.argv[1]
+except IndexError:
+	print(f"Error, expected 1 argument, received {len(sys.argv)-1}")
+	exit()
+
+#initialize date
+date = [0,0,0]
+
+#cases:
+#date formatted like: 1(st) January 2019
+format1="([\\d]+)[\\w]*[\\s]*([\\w]*)[\\s]*([\\d]*)"
+
+#date formatted like: 1 1 2019
+format2="^[\\s]*([\\d]*)[\\s]*([\\d]*)[\\s]*([\\d]*)[\\s]*$"
+
+#next tuesday, last monday
+format3="(next|last)[\\s]*([\\w]*)"
+
+
+
+
+print(parseWeekday(parseme))
